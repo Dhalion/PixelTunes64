5074e0f9c372527cb63899d609ad9d1ba2db7aca
Christopher Falkner <christopher.falkner@web.de>
2025-05-16 18:36:17 +0200

    Batman

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <led-matrix.h>
+#include <graphics.h>
+
+int main() {
+    // Prüfe, ob die Bibliothek verfügbar ist
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
